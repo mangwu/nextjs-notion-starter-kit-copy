bc61de837e2c38a954ff392c80ed047f8ad814de
mangwu <1185956753@qq.com>
2022-07-27 17:09:59 +0800

    修改github链接

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://github.com/mangwu/nextjs-notion-starter-kit-copy'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
