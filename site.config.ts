b0bc9a8aa18f356b1965ea4a84504f4ea1bc5ba5
mangwu <1185956753@qq.com>
2022-07-27 16:58:13 +0800

    notion网站配置修改

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Mang-Wu-Blog-4910d907c9a54b0887e4493647b522da',
+  rootNotionPageId: '4910d907c9a54b0887e4493647b522da',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
