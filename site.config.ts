43a9ca9131538610c5d26c641053aca0d200c61e
mangwu <1185956753@qq.com>
2022-07-28 22:55:39 +0800

    url覆盖

--- site.config.ts
+++ site.config.ts
@@ -45,11 +45,11 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/JavaScript': 'JavaScript-2cd5015d1fd14e10a78aedbc74e60ac7',
+    '/React': 'React-04d002640476440791cefb93895f6dad'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
